b3f2ae9dc4184c1bdf7579f8af39a15722941093
Alireza Azizi <dev.azizimail@gmail.com>
2022-05-03 15:18:55 +0430

    init project with http framework

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+func router(e echo.Echo) {
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+	e.Logger.Fatal(e.Start(":1323"))
+}
+
+func main() {
+
+	echo := echo.New()
+
+	router(*echo)
+
+}
